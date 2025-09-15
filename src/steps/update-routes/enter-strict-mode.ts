41e6503455b9e588c6cedaa776df2887dc2211b8
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-15 15:43:38 +0200

    feature: Entered strict mode

--- src/steps/update-routes/enter-strict-mode.ts
+++ src/steps/update-routes/enter-strict-mode.ts
@@ -1,6 +1,10 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+
 import { doubleColonize } from '@codemod-utils/ember';
 
 import type { AllEntities, Entities, Packages } from '../../types/index.js';
+import { updateTemplateTagFile } from '../../utils/update-components/index.js';
 
 export function enterStrictMode(
   packages: Packages,
@@ -12,9 +16,18 @@ export function enterStrictMode(
     componentsDoubleColonized.set(doubleColonize(entityName), entity);
   }
 
-  console.log(componentsDoubleColonized);
-
   for (const [, packageData] of packages) {
-    console.log(packageData);
+    const { filesWithTemplateTag, packageRoot } = packageData;
+
+    filesWithTemplateTag.routes.forEach((filePath) => {
+      const oldFile = readFileSync(join(packageRoot, filePath), 'utf8');
+
+      const newFile = updateTemplateTagFile(oldFile, {
+        componentsDoubleColonized,
+        entities,
+      });
+
+      writeFileSync(join(packageRoot, filePath), newFile, 'utf8');
+    });
   }
 }
