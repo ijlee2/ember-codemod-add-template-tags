f601aaa4700176acc49d99eac0d458516ec2e5f8
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-15 15:34:20 +0200

    feature: Moved class files

--- src/steps/update-components/move-class-files.ts
+++ src/steps/update-components/move-class-files.ts
@@ -1,7 +1,58 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+import { findFiles, removeFiles } from '@codemod-utils/files';
+
 import type { Packages } from '../../types/index.js';
+import { SOURCE_FOR_INTERNAL_PACKAGES } from '../../utils/ember.js';
+import { insertTemplateTag } from '../../utils/update-components/index.js';
 
 export function moveClassFiles(packages: Packages): void {
   for (const [, packageData] of packages) {
-    console.log(packageData);
+    const { filesWithHBS, packageRoot, packageType } = packageData;
+
+    const source = SOURCE_FOR_INTERNAL_PACKAGES[packageType];
+
+    const classFilePaths = findFiles(`${source}/components/**/*.{js,ts}`, {
+      ignoreList: ['**/*.d.ts'],
+      projectRoot: packageRoot,
+    });
+
+    const classFilePathSet = new Set(classFilePaths);
+
+    // Add the class file to template-only components if missing
+    filesWithHBS.components.forEach((templateFilePath) => {
+      const classFilePathJs = templateFilePath.replace(/\.hbs$/, '.js');
+      const classFilePathTs = templateFilePath.replace(/\.hbs$/, '.ts');
+
+      if (
+        classFilePathSet.has(classFilePathJs) ||
+        classFilePathSet.has(classFilePathTs)
+      ) {
+        return;
+      }
+
+      const newFilePath = templateFilePath.replace(/\.hbs$/, '.gjs');
+
+      const newFile = '<template></template>\n';
+
+      writeFileSync(join(packageRoot, newFilePath), newFile, 'utf8');
+    });
+
+    classFilePaths.forEach((classFilePath) => {
+      const classFile = readFileSync(join(packageRoot, classFilePath), 'utf8');
+
+      const newFilePath = classFilePath.endsWith('.js')
+        ? classFilePath.replace(/\.js$/, '.gjs')
+        : classFilePath.replace(/\.ts$/, '.gts');
+
+      const newFile = insertTemplateTag(classFile);
+
+      writeFileSync(join(packageRoot, newFilePath), newFile, 'utf8');
+    });
+
+    removeFiles(classFilePaths, {
+      projectRoot: packageRoot,
+    });
   }
 }
---
+++ src/utils/update-components/insert-template-tag.ts
@@ -0,0 +1,99 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { AST } from '@codemod-utils/ast-javascript';
+
+import {
+  analyzeComponent,
+  type Data,
+} from '../find-packages-with-hbs/index.js';
+
+function insertToGlimmerComponent(file: string, data: Data): string {
+  const traverse = AST.traverse(true);
+
+  const ast = traverse(file, {
+    visitClassDeclaration(node) {
+      const className = node.value.id.name as string;
+
+      if (className !== data.componentName) {
+        return false;
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      node.value.body.body.push('\n<template></template>');
+
+      return false;
+    },
+  });
+
+  return AST.print(ast);
+}
+
+function insertToTemplateOnlyComponent(file: string, data: Data): string {
+  const traverse = AST.traverse(true);
+
+  const ast = traverse(file, {
+    visitCallExpression(node) {
+      if (node.value.callee.name !== data.baseComponentName) {
+        return false;
+      }
+
+      switch (node.parentPath.value.type) {
+        /*
+          ```ts
+          export default MyComponent = templateOnlyComponent();
+          ```
+        */
+        case 'AssignmentExpression': {
+          node.parentPath.value.right = '<template></template>';
+          break;
+        }
+
+        /*
+          ```ts
+          export default templateOnlyComponent();
+          ```
+        */
+        case 'ExportDefaultDeclaration': {
+          if (node.parentPath.value.id === undefined) {
+            node.parentPath.value.declaration = '<template></template>';
+          }
+          break;
+        }
+
+        /*
+          ```ts
+          const MyComponent = templateOnlyComponent();
+          export default MyComponent;
+          ```
+        */
+        case 'VariableDeclarator': {
+          if (node.parentPath.value.id.name === data.componentName) {
+            node.parentPath.value.init = '<template></template>';
+          }
+          break;
+        }
+      }
+
+      return false;
+    },
+  });
+
+  return AST.print(ast);
+}
+
+export function insertTemplateTag(file: string): string {
+  const data = analyzeComponent(file);
+
+  switch (data.componentType) {
+    case 'glimmer': {
+      return insertToGlimmerComponent(file, data);
+    }
+
+    case 'template-only': {
+      return insertToTemplateOnlyComponent(file, data);
+    }
+
+    default: {
+      return file;
+    }
+  }
+}
