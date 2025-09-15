25aeae32dc569868547fb34f35664a36eecb3348
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-15 15:34:24 +0200

    feature: Moved template files

--- src/steps/update-components/move-template-files.ts
+++ src/steps/update-components/move-template-files.ts
@@ -1,7 +1,57 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+import {
+  findTemplateTags,
+  replaceTemplateTag,
+  type TemplateTag,
+} from '@codemod-utils/ast-template-tag';
+import { findFiles, removeFiles } from '@codemod-utils/files';
+
 import type { Packages } from '../../types/index.js';
+import { SOURCE_FOR_INTERNAL_PACKAGES } from '../../utils/ember.js';
 
 export function moveTemplateFiles(packages: Packages): void {
   for (const [, packageData] of packages) {
-    console.log(packageData);
+    const { filesWithHBS, filesWithTemplateTag, packageRoot, packageType } =
+      packageData;
+
+    const source = SOURCE_FOR_INTERNAL_PACKAGES[packageType];
+
+    const classFilePaths = findFiles(`${source}/components/**/*.{gjs,gts}`, {
+      projectRoot: packageRoot,
+    });
+
+    const classFilePathSet = new Set(classFilePaths);
+
+    filesWithHBS.components.forEach((templateFilePath) => {
+      const classFilePathGjs = templateFilePath.replace(/\.hbs$/, '.gjs');
+      const classFilePathGts = templateFilePath.replace(/\.hbs$/, '.gts');
+
+      const classFilePath = classFilePathSet.has(classFilePathGjs)
+        ? classFilePathGjs
+        : classFilePathGts;
+
+      let classFile = readFileSync(join(packageRoot, classFilePath), 'utf8');
+      const templateTags = findTemplateTags(classFile) as [TemplateTag];
+
+      const templateFile = readFileSync(
+        join(packageRoot, templateFilePath),
+        'utf8',
+      );
+
+      classFile = replaceTemplateTag(classFile, {
+        code: `<template>\n${templateFile}\n</template>`,
+        range: templateTags[0].range,
+      });
+
+      writeFileSync(join(packageRoot, classFilePath), classFile, 'utf8');
+
+      filesWithTemplateTag.components.push(classFilePath);
+    });
+
+    removeFiles(filesWithHBS.components, {
+      projectRoot: packageRoot,
+    });
   }
 }
