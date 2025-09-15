86881f467ae25f17ec4b1a73b0f5076eff0b8b63
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-15 15:43:20 +0200

    feature: Moved template files

--- src/steps/update-routes/move-template-files.ts
+++ src/steps/update-routes/move-template-files.ts
@@ -1,7 +1,33 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+import { removeFiles } from '@codemod-utils/files';
+
 import type { Packages } from '../../types/index.js';
 
 export function moveTemplateFiles(packages: Packages): void {
   for (const [, packageData] of packages) {
-    console.log(packageData);
+    const { filesWithHBS, filesWithTemplateTag, packageRoot } = packageData;
+
+    filesWithHBS.routes.forEach((templateFilePath) => {
+      const classFilePath = templateFilePath.replace(/\.hbs$/, '.gjs');
+
+      const templateFile = readFileSync(
+        join(packageRoot, templateFilePath),
+        'utf8',
+      );
+
+      let classFile = `<template>\n${templateFile}\n</template>`;
+
+      classFile += '\n';
+
+      writeFileSync(join(packageRoot, classFilePath), classFile, 'utf8');
+
+      filesWithTemplateTag.routes.push(classFilePath);
+    });
+
+    removeFiles(filesWithHBS.routes, {
+      projectRoot: packageRoot,
+    });
   }
 }
