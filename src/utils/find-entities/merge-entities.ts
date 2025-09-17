c8db7e33d33525537aa490d638e36f3de8708561
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-17 22:18:52 +0200

    feature: Warned name conflicts

--- src/utils/find-entities/merge-entities.ts
+++ src/utils/find-entities/merge-entities.ts
@@ -4,12 +4,19 @@ import { ENTITY_TYPES } from '../ember.js';
 export function mergeEntities(
   allEntities: AllEntities,
   dependencies: Dependencies,
+  isExternal: boolean,
 ): void {
   ENTITY_TYPES.forEach((entityType) => {
-    for (const [, packageData] of dependencies) {
+    for (const [packageName, packageData] of dependencies) {
       const entities = allEntities[entityType];
 
       for (const [entityName, entity] of packageData.entities[entityType]) {
+        if (!isExternal && entities.get(entityName)) {
+          console.warn(
+            `WARNING: Multiple ${entityType} named ${entityName} exist. Import statements in ${packageName} may be incorrect.`,
+          );
+        }
+
         entities.set(entityName, entity);
       }
     }
