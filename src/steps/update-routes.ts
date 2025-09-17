4fe81c1ec8c39f4401593af3a81ed282aec6cb95
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-17 20:40:11 +0200

    refactor: Improved runtime (update-routes)

--- src/steps/update-routes.ts
+++ src/steps/update-routes.ts
@@ -1,12 +1,14 @@
-import type { AllEntities, Packages } from '../types/index.js';
+import type { AllEntities, FilesCached, Packages } from '../types/index.js';
 import { enterStrictMode, moveTemplateFiles } from './update-routes/index.js';
 
 export function updateRoutes(packages: Packages, entities: AllEntities): void {
+  const filesCached: FilesCached = new Map();
+
   console.time('moveTestFiles');
-  moveTemplateFiles(packages);
+  moveTemplateFiles(packages, filesCached);
   console.timeEnd('moveTestFiles');
 
   console.time('enterStrictMode');
-  enterStrictMode(packages, entities);
+  enterStrictMode(packages, entities, filesCached);
   console.timeEnd('enterStrictMode');
 }
