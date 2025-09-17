f4e56d0646fc7ff20eab44bab6fc38ed0d54eaff
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-17 18:09:08 +0200

    feature: Created removeTemplateOnlyComponentImport()

--- src/utils/update-components/remove-template-only-component-import.ts
+++ src/utils/update-components/remove-template-only-component-import.ts
@@ -5,6 +5,54 @@ type Data = {
   isTypeScript: boolean;
 };
 
+type SpecifierForJavaScript =
+  | {
+      importKind: undefined;
+      imported: undefined;
+      local: {
+        name: string;
+        type: string;
+      };
+      type: 'ImportDefaultSpecifier';
+    }
+  | {
+      importKind: undefined;
+      imported: {
+        name: string;
+        type: string;
+      };
+      local: {
+        name: string;
+        type: string;
+      };
+      type: 'ImportSpecifier';
+    };
+
+type SpecifierForTypeScript =
+  | {
+      importKind: undefined;
+      imported: undefined;
+      local: {
+        name: string;
+        type: string;
+      };
+      type: 'ImportDefaultSpecifier';
+    }
+  | {
+      importKind: 'type' | 'value';
+      imported: {
+        name: string;
+        type: string;
+      };
+      local: {
+        name: string;
+        type: string;
+      };
+      type: 'ImportSpecifier';
+    };
+
+type Specifier = SpecifierForJavaScript | SpecifierForTypeScript;
+
 export function removeTemplateOnlyComponentImport(
   file: string,
   data: Data,
@@ -27,6 +75,21 @@ export function removeTemplateOnlyComponentImport(
 
       if (importPath !== '@ember/component/template-only') {
         return false;
+      }
+
+      node.value.specifiers = (node.value.specifiers as Specifier[]).filter(
+        (specifier) => {
+          const { local, type } = specifier;
+
+          const hasTemplateOnlyComponent =
+            type === 'ImportDefaultSpecifier' && local.type === 'Identifier';
+
+          return !hasTemplateOnlyComponent;
+        },
+      );
+
+      if (node.value.specifiers.length === 0) {
+        return null;
       }
 
       return false;
