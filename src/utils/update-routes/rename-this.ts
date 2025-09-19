410c79ced55e9313c3d67d573e78e7292179fe68
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-19 06:28:05 +0200

    feature: Renamed this in route templates

--- src/utils/update-routes/rename-this.ts
+++ src/utils/update-routes/rename-this.ts
@@ -4,7 +4,15 @@ export function renameThis(file: string): string {
   const traverse = AST.traverse();
 
   const ast = traverse(file, {
-    // ...
+    PathExpression(node) {
+      if (!node.this) {
+        return;
+      }
+
+      const newName = node.original.replace('this.', '@controller.');
+
+      return AST.builders.path(newName);
+    },
   });
 
   return AST.print(ast);
