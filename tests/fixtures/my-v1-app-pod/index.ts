177ac7a1d63d5339f621a1fcd87656f8487864d8
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-24 09:20:12 +0200

    Revert "chore: Added fixtures"
    This reverts commit 6190bdafb0fd3e3e18dee31a4b26c1e71cf12b27.

--- tests/fixtures/my-v1-app-pod/index.ts
+++
@@ -1,6 +0,0 @@
-import { convertFixtureToJson } from '@codemod-utils/tests';
-
-const inputProject = convertFixtureToJson('my-v1-app-pod/input');
-const outputProject = convertFixtureToJson('my-v1-app-pod/output');
-
-export { inputProject, outputProject };
--- tests/fixtures/my-v1-app-pod/output/app/components/products/product/image/component.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import config from 'docs-app/config/environment';
-
-import styles from './styles.css';
-
-export default class ProductsProductImageComponent extends Component {
-  isTestEnvironment = config.environment === 'test';
-  styles = styles;
-}
