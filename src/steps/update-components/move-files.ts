3c1eaf31965795348e4ae4b2427bec1edacc6702
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-28 08:55:03 +0200

    refactor: Removed a loop of file reads

--- src/steps/update-components/move-files.ts
+++ src/steps/update-components/move-files.ts
@@ -1,6 +1,11 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
+import {
+  findTemplateTags,
+  replaceTemplateTag,
+  type TemplateTag,
+} from '@codemod-utils/ast-template-tag';
 import { findFiles, removeFiles } from '@codemod-utils/files';
 
 import type { Packages } from '../../types/index.js';
@@ -9,51 +14,75 @@ import { insertTemplateTag } from '../../utils/update-components/index.js';
 
 export function moveFiles(packages: Packages): void {
   for (const [, packageData] of packages) {
-    const { filesWithHBS, packageRoot, packageType } = packageData;
+    const { filesWithHBS, filesWithTemplateTag, packageRoot, packageType } =
+      packageData;
 
     const source = SOURCE_FOR_INTERNAL_PACKAGES[packageType];
 
-    const classFilePaths = findFiles(`${source}/components/**/*.{js,ts}`, {
-      ignoreList: ['**/*.d.ts'],
-      projectRoot: packageRoot,
-    });
+    const classFilePaths = new Set(
+      findFiles(`${source}/components/**/*.{js,ts}`, {
+        ignoreList: ['**/*.d.ts'],
+        projectRoot: packageRoot,
+      }),
+    );
 
-    const classFilePathSet = new Set(classFilePaths);
     const classFilePathsToRemove: string[] = [];
 
     filesWithHBS.components.forEach((templateFilePath) => {
       const classFilePathJs = templateFilePath.replace(/\.hbs$/, '.js');
       const classFilePathTs = templateFilePath.replace(/\.hbs$/, '.ts');
 
-      let classFilePath: string | undefined;
+      let classFilePath: string;
+      let classFile: string;
 
-      if (classFilePathSet.has(classFilePathJs)) {
+      if (classFilePaths.has(classFilePathJs)) {
         classFilePath = classFilePathJs;
-      } else if (classFilePathSet.has(classFilePathTs)) {
+        classFile = readFileSync(join(packageRoot, classFilePath), 'utf8');
+        classFilePathsToRemove.push(classFilePath);
+      } else if (classFilePaths.has(classFilePathTs)) {
         classFilePath = classFilePathTs;
+        classFile = readFileSync(join(packageRoot, classFilePath), 'utf8');
+        classFilePathsToRemove.push(classFilePath);
+      } else {
+        // If a component consists of *.hbs only
+        classFilePath = classFilePathJs;
+        classFile = [
+          `import templateOnlyComponent from '@ember/component/template-only';`,
+          ``,
+          `export default templateOnlyComponent();`,
+          ``,
+        ].join('\n');
       }
 
-      if (!classFilePath) {
-        return;
-      }
-
-      const classFile = readFileSync(join(packageRoot, classFilePath), 'utf8');
       const isTypeScript = classFilePath.endsWith('.ts');
 
-      const newFilePath = isTypeScript
+      classFilePath = isTypeScript
         ? classFilePath.replace(/\.ts$/, '.gts')
         : classFilePath.replace(/\.js$/, '.gjs');
 
-      const newFile = insertTemplateTag(classFile, {
+      classFile = insertTemplateTag(classFile, {
         isTypeScript,
+      })!;
+
+      const templateTags = findTemplateTags(classFile) as [TemplateTag];
+
+      const templateFile = readFileSync(
+        join(packageRoot, templateFilePath),
+        'utf8',
+      );
+
+      classFile = replaceTemplateTag(classFile, {
+        code: `<template>\n${templateFile}\n</template>`,
+        range: templateTags[0].range,
       });
 
-      if (!newFile) {
-        return;
-      }
+      writeFileSync(join(packageRoot, classFilePath), classFile, 'utf8');
 
-      classFilePathsToRemove.push(classFilePath);
-      writeFileSync(join(packageRoot, newFilePath), newFile, 'utf8');
+      filesWithTemplateTag.components.push(classFilePath);
+    });
+
+    removeFiles(filesWithHBS.components, {
+      projectRoot: packageRoot,
     });
 
     removeFiles(classFilePathsToRemove, {
