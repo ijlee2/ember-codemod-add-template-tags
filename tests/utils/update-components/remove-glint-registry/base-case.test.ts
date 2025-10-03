1fa92d48c96996761f6988d3931546027ccb26e4
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-10-03 10:52:29 +0200

    refactor: Added tests

---
+++ tests/utils/update-components/remove-glint-registry/base-case.test.ts
@@ -0,0 +1,31 @@
+import { assert, test } from '@codemod-utils/tests';
+
+import { removeGlintRegistry } from '../../../../src/utils/update-components/index.js';
+
+test('utils | update-components | remove-glint-registry > v1-app', function () {
+  const oldFile = [
+    `import Component from '@glimmer/component';`,
+    ``,
+    `interface UiFormSignature { /* ... */ }`,
+    ``,
+    `export default class UiFormComponent extends Component<UiFormSignature> {}`,
+    ``,
+  ].join('\n');
+
+  const newFile = removeGlintRegistry(oldFile, {
+    isTypeScript: true,
+    packageType: 'v1-app',
+  });
+
+  assert.strictEqual(
+    newFile,
+    [
+      `import Component from '@glimmer/component';`,
+      ``,
+      `interface UiFormSignature { /* ... */ }`,
+      ``,
+      `export default class UiFormComponent extends Component<UiFormSignature> {}`,
+      ``,
+    ].join('\n'),
+  );
+});
---
+++ tests/utils/update-components/remove-glint-registry/v2-addon.test.ts
@@ -0,0 +1,28 @@
+import { assert, test } from '@codemod-utils/tests';
+
+import { removeGlintRegistry } from '../../../../src/utils/update-components/index.js';
+
+test('utils | update-components | remove-glint-registry > v2-addon', function () {
+  const oldFile = [
+    `import Component from '@glimmer/component';`,
+    ``,
+    `interface UiFormSignature { /* ... */ }`,
+    ``,
+    `export default class UiFormComponent extends Component<UiFormSignature> {}`,
+    ``,
+    `declare module '@glint/environment-ember-loose/registry' {`,
+    `  export default interface Registry {`,
+    `    'Ui::Form': typeof UiFormComponent;`,
+    `    'ui/form': typeof UiFormComponent;`,
+    `  }`,
+    `}`,
+    ``,
+  ].join('\n');
+
+  const newFile = removeGlintRegistry(oldFile, {
+    isTypeScript: true,
+    packageType: 'v2-addon',
+  });
+
+  assert.strictEqual(newFile, oldFile);
+});
