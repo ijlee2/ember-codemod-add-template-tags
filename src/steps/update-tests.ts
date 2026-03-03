cb70e7a66e69468f79e24de7b24bf4d670ed2d05
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-03 21:44:52 +0100

    feature: Parallelized code (update-tests)

--- src/steps/update-tests.ts
+++ src/steps/update-tests.ts
@@ -1,11 +1,10 @@
 import type { AllEntities, Packages } from '../types/index.js';
 import { enterStrictMode, moveFiles } from './update-tests/index.js';
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export async function updateTests(
   packages: Packages,
   entities: AllEntities,
 ): Promise<void> {
-  moveFiles(packages);
-  enterStrictMode(packages, entities);
+  await moveFiles(packages);
+  await enterStrictMode(packages, entities);
 }
